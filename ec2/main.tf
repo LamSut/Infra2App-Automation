fa173b77f5a0c68ee14c6bb22ffe777e336ff6d5
Lam Sut <lamsut0007@gmail.com>
2025-03-16 09:08:23 +0000

    output public ips

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "amazon" {
   provisioner "remote-exec" {
     inline = [
       "sudo dnf update -y",
+      "sudo dnf install -y python3 python3-pip",
       "sudo dnf install -y amazon-linux-extras",
       "sudo amazon-linux-extras enable ansible2",
       "sudo dnf install -y ansible"
--- main.tf
+++ main.tf
@@ -8,3 +8,12 @@ module "ec2" {
   private_subnet = module.vpc.private_subnet_ids
   security_group = module.vpc.sg_ids
 }
+
+output "all_public_ips" {
+  description = "Public IPs of all instances"
+  value = {
+    amazon  = module.ec2.amazon_public_ips
+    ubuntu  = module.ec2.ubuntu_public_ips
+    windows = module.ec2.windows_public_ips
+  }
+}
