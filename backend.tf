a8d8288145786812fce940b84740518233f8120d
Lam Sut <lamsut0007@gmail.com>
2025-03-22 13:54:55 +0000

    refactor into terrform & ansible

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "b2111933-tfbucket"
-    key            = "b2111933-tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "b2111933-tftable"
-  }
-}
