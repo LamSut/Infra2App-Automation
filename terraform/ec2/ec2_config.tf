3166112a3abe6531aabb5f54a8e5b280955c5e25
Lam Sut <lamsut0007@gmail.com>
2025-06-07 12:59:59 +0000

    wtf

--- terraform/ec2/ec2_config.tf
+++ terraform/ec2/ec2_config.tf
@@ -17,12 +17,12 @@ locals {
   amazon_playbooks = [
     "${var.pb_linux_path}/hack-website/install.yaml",
   ]
-  ubuntu_playbooks = [
-    "${var.pb_linux_path}/pizza-website/install.yaml",
-  ]
-  windows_playbooks = [
-    "${var.pb_windows_path}/nginx/install.yaml"
-  ]
+  # ubuntu_playbooks = [
+  #   "${var.pb_linux_path}/pizza-website/install.yaml",
+  # ]
+  # windows_playbooks = [
+  #   "${var.pb_windows_path}/nginx/install.yaml"
+  # ]
 }
 
 resource "null_resource" "amazon_config" {
@@ -45,50 +45,50 @@ resource "null_resource" "amazon_config" {
   }
 }
 
-resource "null_resource" "ubuntu_config" {
-  count = length(local.linux_public_ips)
-  depends_on = [
-    aws_instance.ubuntu,
-  ]
+# resource "null_resource" "ubuntu_config" {
+#   count = length(local.linux_public_ips)
+#   depends_on = [
+#     aws_instance.ubuntu,
+#   ]
 
-  provisioner "local-exec" {
-    command = join(" && ", concat(
-      ["echo \"Running Ansible playbooks on Ubuntu instance with IP: ${local.linux_public_ips[count.index]}\""],
-      [for pb in local.ubuntu_playbooks : format(
-        "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u %s --key-file %s -T 300 -i '%s,' %s",
-        local.linux_users[count.index],
-        var.private_key_path,
-        local.linux_public_ips[count.index],
-        pb
-      )]
-    ))
-  }
-}
+#   provisioner "local-exec" {
+#     command = join(" && ", concat(
+#       ["echo \"Running Ansible playbooks on Ubuntu instance with IP: ${local.linux_public_ips[count.index]}\""],
+#       [for pb in local.ubuntu_playbooks : format(
+#         "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u %s --key-file %s -T 300 -i '%s,' %s",
+#         local.linux_users[count.index],
+#         var.private_key_path,
+#         local.linux_public_ips[count.index],
+#         pb
+#       )]
+#     ))
+#   }
+# }
 
-resource "null_resource" "windows_config" {
-  count      = length(local.windows_public_ips)
-  depends_on = [aws_instance.windows]
+# resource "null_resource" "windows_config" {
+#   count      = length(local.windows_public_ips)
+#   depends_on = [aws_instance.windows]
 
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Waiting for Windows startup (60s)..."
-      sleep 60
-      while ! nc -z ${local.windows_public_ips[count.index]} 5986; do
-        echo "Waiting for Windows to be ready..."
-        sleep 10
-      done
-      echo "Windows is ready!"
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       echo "Waiting for Windows startup (60s)..."
+#       sleep 60
+#       while ! nc -z ${local.windows_public_ips[count.index]} 5986; do
+#         echo "Waiting for Windows to be ready..."
+#         sleep 10
+#       done
+#       echo "Windows is ready!"
 
-      echo "Retrieving Windows password..."
-      aws ec2 get-password-data --instance-id ${aws_instance.windows[count.index].id} --priv-launch-key ${var.private_key_path} --output text | tr -d '\r\n' > ../keys/${aws_instance.windows[count.index].id}.txt
+#       echo "Retrieving Windows password..."
+#       aws ec2 get-password-data --instance-id ${aws_instance.windows[count.index].id} --priv-launch-key ${var.private_key_path} --output text | tr -d '\r\n' > ../keys/${aws_instance.windows[count.index].id}.txt
 
-      echo "Running Ansible playbooks on IP: ${local.windows_public_ips[count.index]}"
-      for pb in ${join(" ", local.windows_playbooks)}; do
-        ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-          -u ${local.windows_users[count.index]} --connection=winrm \
-          --extra-vars "ansible_winrm_server_cert_validation=ignore ansible_winrm_password=$(awk '{print $2}' ../keys/${aws_instance.windows[count.index].id}.txt | tr -d '\r')" \
-          -T 600 -i '${local.windows_public_ips[count.index]},' "$pb"
-      done
-    EOT
-  }
-}
+#       echo "Running Ansible playbooks on IP: ${local.windows_public_ips[count.index]}"
+#       for pb in ${join(" ", local.windows_playbooks)}; do
+#         ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+#           -u ${local.windows_users[count.index]} --connection=winrm \
+#           --extra-vars "ansible_winrm_server_cert_validation=ignore ansible_winrm_password=$(awk '{print $2}' ../keys/${aws_instance.windows[count.index].id}.txt | tr -d '\r')" \
+#           -T 600 -i '${local.windows_public_ips[count.index]},' "$pb"
+#       done
+#     EOT
+#   }
+# }
