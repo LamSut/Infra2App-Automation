34b972622b9b3cd9f6963f27398332962e9a4c99
Truong Dang Truc Lam <116415510+LamSut@users.noreply.github.com>
2025-07-04 22:27:02 +0700

    migrate to another account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "b2111933-tfbucket"
-    key            = "b2111933-tfstate"
+    bucket         = "b2111933-bucket"
+    key            = "b2111933-state"
     region         = "us-east-1"
-    dynamodb_table = "b2111933-tftable"
+    dynamodb_table = "b2111933-table"
   }
 }
