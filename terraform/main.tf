cb6ee99a0ee82c0519d8da24e4095c786355453f
LamSut <lamsut0007@gmail.com>
2025-07-28 18:49:05 +0700

    nil hil

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,19 @@
-######################
-### Cloud Provider ###
-######################
+###############################
+### Terraform Configuration ###
+###############################
 
 provider "aws" {
   region = "ap-southeast-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "b2111933-store"
+    key            = "b2111933-state"
+    region         = "ap-southeast-1"
+    dynamodb_table = "b2111933-lock"
+  }
+}
 
 ##################
 ### VPC Module ###
@@ -56,25 +64,4 @@ module "ec2" {
   amazon_playbooks  = var.amazon_playbooks
   ubuntu_playbooks  = var.ubuntu_playbooks
   windows_playbooks = var.windows_playbooks
-}
-
-
-###############
-### Outputs ###
-###############
-
-output "all_public_ips" {
-  description = "Public IPs of all instances"
-
-  value = {
-    hack       = module.ec2.hack_public_ip
-    hack_admin = module.ec2.hack_admin_url # Username: root, Password empty
-
-    pizza       = module.ec2.pizza_public_ip
-    pizza_admin = module.ec2.pizza_admin_url
-
-    ec2_amazon  = module.ec2.amazon_public_ips
-    ec2_ubuntu  = module.ec2.ubuntu_public_ips
-    ec2_windows = module.ec2.windows_public_ips
-  }
 }
